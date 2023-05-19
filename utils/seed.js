ab0780180d66dfa68b8bf6a7b1668901ba596da3
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-05-19 15:58:21 -0400

    Added seeds for users, initiated npm and added connection.js

---
+++ utils/seed.js
@@ -0,0 +1,34 @@
+const connection = require("../config/connection");
+const { User, Thought, Reaction } = require("../models");
+
+const usersSeed = [
+  {
+    username: "user-a",
+    email: "user-a@gmail.com",
+  },
+  {
+    username: "user-b",
+    email: "user-b@gmail.com",
+  },
+  {
+    username: "user-c",
+    email: "user-c@gmail.com",
+  },
+  {
+    username: "user-d",
+    email: "user-d@gmail.com",
+  },
+  {
+    username: "user-e",
+    email: "user-e@gmail.com",
+  },
+];
+
+connection.once("open", async () => {
+  await User.deleteMany({});
+
+  await User.collection.insertMany(usersSeed);
+
+  console.info("Seeding complete! 🌱");
+  process.exit(0);
+});
